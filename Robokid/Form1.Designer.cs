b59777e5b498d970cbc6f206d24da80a51e4882a
Ahmed Osama Afefy <32176872+ahmedosama07@users.noreply.github.com>
2019-03-18 12:03:27 +0200

    Add files via upload

---
+++ Robokid/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Robokid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.arm = new System.Windows.Forms.GroupBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.gripper = new System.Windows.Forms.GroupBox();
+            this.close = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.serial_connection = new System.Windows.Forms.GroupBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.controlpan = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.arm.SuspendLayout();
+            this.gripper.SuspendLayout();
+            this.serial_connection.SuspendLayout();
+            this.controlpan.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Location = new System.Drawing.Point(484, 176);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(438, 100);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Megnets";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(332, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Box 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(192, 78);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(52, 17);
+            this.radioButton5.TabIndex = 16;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Box 5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(48, 78);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(52, 17);
+            this.radioButton6.TabIndex = 17;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Box 6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(192, 28);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Box 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(48, 28);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(52, 17);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Box 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(332, 78);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(52, 17);
+            this.radioButton4.TabIndex = 15;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Box 4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // arm
+            // 
+            this.arm.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.arm.Controls.Add(this.button10);
+            this.arm.Controls.Add(this.button9);
+            this.arm.Location = new System.Drawing.Point(446, 108);
+            this.arm.Name = "arm";
+            this.arm.Size = new System.Drawing.Size(184, 49);
+            this.arm.TabIndex = 11;
+            this.arm.TabStop = false;
+            this.arm.Text = "Arm";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(104, 15);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(67, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Shrink";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(16, 15);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(67, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Stretch";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // gripper
+            // 
+            this.gripper.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.gripper.Controls.Add(this.close);
+            this.gripper.Controls.Add(this.open);
+            this.gripper.Location = new System.Drawing.Point(442, 24);
+            this.gripper.Name = "gripper";
+            this.gripper.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.gripper.Size = new System.Drawing.Size(181, 50);
+            this.gripper.TabIndex = 10;
+            this.gripper.TabStop = false;
+            this.gripper.Text = "Gripper";
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(92, 15);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(74, 23);
+            this.close.TabIndex = 9;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(4, 15);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(74, 23);
+            this.open.TabIndex = 8;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // serial_connection
+            // 
+            this.serial_connection.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.serial_connection.Controls.Add(this.button12);
+            this.serial_connection.Controls.Add(this.comboBox1);
+            this.serial_connection.Controls.Add(this.button1);
+            this.serial_connection.Location = new System.Drawing.Point(12, 12);
+            this.serial_connection.Name = "serial_connection";
+            this.serial_connection.Size = new System.Drawing.Size(346, 100);
+            this.serial_connection.TabIndex = 2;
+            this.serial_connection.TabStop = false;
+            this.serial_connection.Text = "Serial Connection";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(12, 68);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(83, 23);
+            this.button12.TabIndex = 6;
+            this.button12.Text = "Serial Monitor";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(101, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(232, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // controlpan
+            // 
+            this.controlpan.Controls.Add(this.button8);
+            this.controlpan.Controls.Add(this.button7);
+            this.controlpan.Location = new System.Drawing.Point(12, 174);
+            this.controlpan.Name = "controlpan";
+            this.controlpan.Size = new System.Drawing.Size(441, 100);
+            this.controlpan.TabIndex = 4;
+            this.controlpan.TabStop = false;
+            this.controlpan.Text = "Control";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(254, 19);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(162, 63);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "Autonomous";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(36, 19);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(162, 63);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Manual";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Robokid.Properties.Resources.back;
+            this.ClientSize = new System.Drawing.Size(934, 288);
+            this.Controls.Add(this.arm);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gripper);
+            this.Controls.Add(this.controlpan);
+            this.Controls.Add(this.serial_connection);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Robokid";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.arm.ResumeLayout(false);
+            this.gripper.ResumeLayout(false);
+            this.serial_connection.ResumeLayout(false);
+            this.controlpan.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox serial_connection;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox arm;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.GroupBox gripper;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.GroupBox controlpan;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ Robokid/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using DarrenLee.Media;
+using AForge.Video;
+
+namespace Robokid
+{
+    
+    public partial class Form1 : Form
+    {
+        bool isManual = false;
+        bool isConnected = false;
+        String[] ports;
+        SerialPort port;
+
+        int rightSpeed = 0, leftSpeed = 0, forwardSpeed = 0, backwardSpeed = 0;
+        string rightProtocol = "", leftProtocol = "", forwardProtocol = "", backwardProtocol = "";
+        int[] speed = new int[4];
+        string[] protocols = new string[4];
+        int count = 0;
+        Camera mycamera = new Camera();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //KeyDown += new KeyEventHandler(Form1_KeyDown);
+
+            disableControls();
+            getAvailableComPorts();
+
+            button1.Enabled = true;
+
+            //.BackgroundImageLayout = ImageLayout.Stretch;
+            serial_connection.BackColor = Color.Transparent;
+            controlpan.BackColor = Color.Transparent;
+
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                {
+                    comboBox1.SelectedItem = ports[0];
+                }
+            }
+
+            
+
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                connectToArduino();
+                enableControls();
+            }
+            else
+            {
+                disconnectFromArduino();
+            }
+        }
+
+        void getAvailableComPorts()
+        {
+            ports = SerialPort.GetPortNames();
+        }
+
+        private void connectToArduino()
+        {
+            isConnected = true;
+            string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
+            port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            port.Write("#STAR\n");
+            button1.Text = "Disconnect";
+            enableControls();
+        }
+
+        private void disconnectFromArduino()
+        {
+            isConnected = false;
+            port.Write("#STOP\n");
+            port.Close();
+            button1.Text = "Connect";
+            disableControls();
+            resetDefaults();
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                port.Write("#MOVELeft\n");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                port.Write("#MOVEForward\n");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                port.Write("#MOVERight\n");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                port.Write("#MOVEBackward\n");
+            }
+        }
+
+        private void enableControls()
+        {
+            open.Enabled = true;
+            close.Enabled = true;
+            button9.Enabled = true;
+            button10.Enabled = true;
+            serial_connection.Enabled = true;
+            controlpan.Enabled = true;
+            button12.Enabled = true;
+        }
+
+        private void disableControls()
+        {
+            open.Enabled = false;
+            close.Enabled = false;
+            button9.Enabled = false;
+            button10.Enabled = false;
+            controlpan.Enabled = false;
+        }
+
+        private void resetDefaults()
+        {
+            button7.BackColor = Color.Gray;
+            button8.BackColor = Color.Gray;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        private void motion_pan_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                port.Write("#GRBR" + "OPEN" + "#\n");
+                open.Enabled = false;
+                close.Enabled = true;
+            }
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                port.Write("#GRBR" + "CLOSE" + "#\n");
+                close.Enabled = false;
+                open.Enabled = true;
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            }
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            Console.WriteLine(port.ReadByte());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+}
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            port.Write("G");
+            port.Write("1");
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            port.Write("G");
+            port.Write("2");
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            port.Write("G");
+            port.Write("3");
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            port.Write("G");
+            port.Write("4");
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            port.Write("G");
+            port.Write("5");
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            port.Write("G");
+            port.Write("6");
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (isConnected && isManual)
+            {
+
+                if (keyData == Keys.Up)
+                {
+                    if (forwardSpeed < 255)
+                    {
+                        forwardSpeed++;
+                    }
+                    forwardProtocol = "#MOVEForward" + forwardSpeed.ToString() + "\n";
+                    port.Write("u");
+                    return true;
+                }
+                
+
+                if (keyData == Keys.Down)
+                {
+                    if (backwardSpeed < 255)
+                    {
+                        backwardSpeed++;
+                    }
+                    backwardProtocol = "#MOVEBackward" + backwardSpeed.ToString() + "\n";
+                    port.Write("b");
+                    return true;
+                }
+
+                if (keyData == Keys.Right)
+                {
+                    if (rightSpeed < 255)
+                    {
+                        rightSpeed++;
+                    }
+                    rightProtocol = "#MOVERight" + rightSpeed.ToString() + "\n";
+                    port.Write("r");
+                    return true;
+                }
+
+                if (keyData == Keys.Left)
+                {
+                    if (leftSpeed < 255)
+                    {
+                        leftSpeed++;
+                    }
+                    leftProtocol = "#MOVELeft" + leftSpeed.ToString() + "\n";
+                    port.Write("l");
+                    return true;
+                }
+            }
+            port.Write("R");
+            //textBox1.AppendText(forwardSpeed + " , " + backwardSpeed + " , " + rightSpeed + " , " + leftSpeed);
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                isManual = false;
+                port.Write("A");
+                button8.BackColor = Color.Green;
+                button7.BackColor = Color.Red;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                isManual = true;
+                port.Write("M");
+                button7.BackColor = Color.Green;
+                button8.BackColor = Color.Red;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                port.Write("#ARM" + "STRTSH" + "#\n");
+                button9.Enabled = false;
+                button10.Enabled = true;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                port.Write("#ARM" + "SHRNK" + "#\n");
+                button10.Enabled = false;
+                button9.Enabled = true;
+            }
+        }
+        
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
